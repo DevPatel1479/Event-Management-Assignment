f99026ed105820f5f1592a823e632d0910f41333
Dev Patel <devpatel1828king@gmail.com>
2025-10-30 20:22:07 +0530

    updated urls.py for event view

--- events/urls.py
+++ events/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
+from .views import EventViewSet, RSVPViewSet, RSVPUpdateView,  ReviewViewSet
 
 router = DefaultRouter()
-
+router.register(r'events', EventViewSet, basename='event')
 
 urlpatterns = [
+    path('', include(router.urls)),
     
+
 ]
-
-
